f6f7f0ecf3803ef2da1b17f7f8afcdd8486412bd
plunestechnologies@gmail.com <plunestechnologies@gmail.com>
2020-03-25 16:16:22 +0530

    flutter upgraded,project working on both platforms properly

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/manav/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/manav/Documents/PlunesApp"
-export "FLUTTER_TARGET=/Users/manav/Documents/PlunesApp/lib/main.dart"
+export "FLUTTER_ROOT=/Users/craterzone/Documents/systemdata/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/craterzone/Downloads/PlunesApp_Prod"
+export "FLUTTER_TARGET=/Users/craterzone/Downloads/PlunesApp_Prod/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/manav/Downloads/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.2.15"
-export "FLUTTER_BUILD_NUMBER=53"
+export "FLUTTER_FRAMEWORK_DIR=/Users/craterzone/Documents/systemdata/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.2.16"
+export "FLUTTER_BUILD_NUMBER=54"
 export "TRACK_WIDGET_CREATION=true"
